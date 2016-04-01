f4ea028dd25f0555ef77fe63c72e8cbd32a08ee5
qianfeng <qianfeng@1.local>
2016-04-01 14:50:18 +0800

    第一次创建项目

---
+++ gittdemo2/gittdemo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gittdemo2
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 何得林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gittdemo2/gittdemo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittdemo2
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 何得林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
